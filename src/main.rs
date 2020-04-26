65e322b61d3da116f5ebe0858741215a4fd8a885
Alifya53 <alifyahussain53@gmail.com>
2020-04-27 00:54:47 +0500

    file updated

--- src/main.rs
+++ src/main.rs
@@ -35,33 +35,66 @@
 
 
 
-use futures::executor::block_on;
-// use:std::thread;
-// use:std::time:Duration;
-async fn learn_song() -> Song {println!("Song learning!")}
-async fn sing_song(song: Song) {println!("Song learning!")}
-async fn dance() { println!("Dancing Time!") }
+// use futures::executor::block_on;
+// // use:std::thread;
+// // use:std::time:Duration;
+// async fn learn_song() -> Song {println!("Song learning!")}
+// async fn sing_song(song: Song) {println!("Song learning!")}
+// async fn dance() { println!("Dancing Time!") }
 
-async fn learn_and_sing() {
-    // Wait until the song has been learned before singing it.
-    // We use `.await` here rather than `block_on` to prevent blocking the
-    // thread, which makes it possible to `dance` at the same time.
-    let song = learn_song().await;
-    sing_song(song).await;
+// async fn learn_and_sing() {
+//     // Wait until the song has been learned before singing it.
+//     // We use `.await` here rather than `block_on` to prevent blocking the
+//     // thread, which makes it possible to `dance` at the same time.
+//     let song = learn_song().await;
+//     sing_song(song).await;
+// }
+
+// async fn async_main() {
+//     let f1 = learn_and_sing();
+//     let f2 = dance();
+
+//     // `join!` is like `.await` but can wait for multiple futures concurrently.
+//     // If we're temporarily blocked in the `learn_and_sing` future, the `dance`
+//     // future will take over the current thread. If `dance` becomes blocked,
+//     // `learn_and_sing` can take back over. If both futures are blocked, then
+//     // `async_main` is blocked and will yield to the executor.
+//     futures::join!(f1, f2);
+// }
+
+// fn main() {
+//     block_on(async_main());
+// }
+
+
+use std::thread;
+use std::time::Duration;
+learn_song ();
+sing_song ();
+dance ();
+fn learn_song() {
+    thread::sleep(Duration::from_secs(2));
+    println!("I learned and Love this song!!");
 }
+fn sing_song(){
+    thread::sleep(Duration::from_secs(3));
+    println!("I am singing this song ! its lovely!!");
+}
+fn dance(){
+    thread::sleep(Duration::from_sec(5));
+    println!("I will dance until someonne stops me!!!")
+}
+ fn learn_and_sing() {
+        // Wait until the song has been learned before singing it.
+        // We use `.await` here rather than `block_on` to prevent blocking the
+        // thread, which makes it possible to `dance` at the same time.
+        let song = learn_song().await;
+        sing_song(song).await;
+    }
 
-async fn async_main() {
+fn main(){
     let f1 = learn_and_sing();
     let f2 = dance();
 
-    // `join!` is like `.await` but can wait for multiple futures concurrently.
-    // If we're temporarily blocked in the `learn_and_sing` future, the `dance`
-    // future will take over the current thread. If `dance` becomes blocked,
-    // `learn_and_sing` can take back over. If both futures are blocked, then
-    // `async_main` is blocked and will yield to the executor.
-    futures::join!(f1, f2);
-}
 
-fn main() {
-    block_on(async_main());
 }
